9b7018021c400ed589b5c4f88f1dcdc27d0d5d5c
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-21 09:52:27 +0200

    Added the coursewareable initializer.

---
+++ config/initializers/coursewareable.rb
@@ -0,0 +1,46 @@
+require 'ostruct'
+
+# Coursewareable configuration entries
+module Coursewareable
+  class << self
+    attr_reader :config
+  end
+
+  @config = OpenStruct.new(
+    :domain_blacklist => %w(blog api support help mail ftp dashboard),
+    :plans => {
+      :free => {
+        :slug => :free,
+        :allowed_classrooms => 1,
+        :allowed_collaborators => 0,
+        :allowed_space => 100.megabytes,
+        :expires_in => nil,
+        :cost => 0
+      },
+      :micro => {
+        :slug => :micro,
+        :allowed_classrooms => 5,
+        :allowed_collaborators => 5,
+        :allowed_space => 5.gigabytes,
+        :expires_in => Time.now + 1.month,
+        :cost => 7
+      },
+      :small => {
+        :slug => :small,
+        :allowed_classrooms => 10,
+        :allowed_collaborators => 10,
+        :allowed_space => 10.gigabytes,
+        :expires_in => Time.now + 1.month,
+        :cost => 12
+      },
+      :medium => {
+        :slug => :medium,
+        :allowed_classrooms => 20,
+        :allowed_collaborators => 20,
+        :allowed_space => 20.gigabytes,
+        :expires_in => Time.now + 1.month,
+        :cost => 22
+      }
+    }
+  )
+end
