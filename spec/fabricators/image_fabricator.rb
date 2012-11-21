4f778d2ecd63376b7a51de54aab63dd465be4cc2
Stas SUȘCOV <stas@net.utcluj.ro>
2012-11-22 01:06:40 +0200

    Use relative path to current file when using fixtures.

--- spec/fabricators/image_fabricator.rb
+++ spec/fabricators/image_fabricator.rb
@@ -2,7 +2,8 @@ include ActionDispatch::TestProcess
 
 Fabricator('coursewareable/image') do
   description   { sequence(:image){ Faker::HipsterIpsum.paragraph } }
-  attachment    { fixture_file_upload('spec/fixtures/test.png', 'image/png') }
+  attachment    { fixture_file_upload(
+    File.expand_path('../../fixtures/test.png', __FILE__), 'image/png' )}
   user(:fabricator => 'coursewareable/user')
   classroom(:fabricator => 'coursewareable/classroom')
   assetable     { |attr| Fabricate('coursewareable/syllabus',
--- spec/fabricators/upload_fabricator.rb
+++ spec/fabricators/upload_fabricator.rb
@@ -2,7 +2,8 @@ include ActionDispatch::TestProcess
 
 Fabricator('coursewareable/upload') do
   description   { sequence(:upload){ Faker::HipsterIpsum.paragraph } }
-  attachment    { fixture_file_upload('spec/fixtures/test.txt', 'text/plain') }
+  attachment    { fixture_file_upload(
+    File.expand_path('../../fixtures/test.txt', __FILE__), 'text/plain') }
   user(:fabricator => 'coursewareable/user')
   classroom(:fabricator => 'coursewareable/classroom')
   assetable     { |attr| Fabricate('coursewareable/syllabus',
