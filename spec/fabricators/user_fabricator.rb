d238785ee99d8d6540904924a8f10d3d82bc88af
Stas SUȘCOV <stas@net.utcluj.ro>
2012-12-16 21:12:33 +0200

    Updated gems, esp. cause of fabrication bugfix. User fabricators cleaned up.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -5,18 +5,10 @@ Fabricator('coursewareable/user') do
   password    'secret'
 end
 
-Fabricator(:admin, :class_name => Coursewareable::User) do
-  email       { sequence(:email) { Faker::Internet.email } }
-  first_name  Faker::Name.first_name
-  last_name   Faker::Name.last_name
-  password    'secret'
+Fabricator(:admin, :from => 'coursewareable/user') do
   role        :admin
 end
 
-Fabricator(:confirmed_user, :class_name => Coursewareable::User) do
-  email       { sequence(:email) { Faker::Internet.email } }
-  first_name  Faker::Name.first_name
-  last_name   Faker::Name.last_name
-  password    'secret'
+Fabricator(:confirmed_user, :from => 'coursewareable/user') do
   after_create { |usr| usr.activate! }
 end
