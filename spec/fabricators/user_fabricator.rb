c7567e8de31ee52b1b6728f962ab9fb6a7f09865
Stas SUȘCOV <stas@net.utcluj.ro>
2012-12-21 19:45:23 +0200

    Closure first_name and last_name fabricator attributes to avoid duplication.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator('coursewareable/user') do
   email       { sequence(:email) { Faker::Internet.email } }
-  first_name  Faker::Name.first_name
-  last_name   Faker::Name.last_name
+  first_name  { Faker::Name.first_name }
+  last_name   { Faker::Name.last_name }
   password    'secret'
 end
 
