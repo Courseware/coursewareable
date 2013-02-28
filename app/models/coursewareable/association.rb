1e5d25ec730887ccf093e11252f84bbed9f18152
Stas SUȘCOV <stas@net.utcluj.ro>
2013-02-28 11:35:04 +0200

    Update membership/collaboration accessible attributes.

--- app/models/coursewareable/association.rb
+++ app/models/coursewareable/association.rb
@@ -5,6 +5,8 @@ module Coursewareable
     store :email_announcement, :accessors => [
       :send_announcements, :send_generic, :send_grades]
 
+    attr_accessible :send_announcements, :send_generic, :send_grades
+
     # Callbacks
     before_create do
       self.send_announcements = true if send_announcements.nil?
