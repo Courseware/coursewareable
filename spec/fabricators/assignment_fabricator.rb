ec41fc05a715ba5021b86be7174c2815f6438772
Stas SUȘCOV <stas@net.utcluj.ro>
2013-03-27 20:01:23 +0200

    Added sanitization to assignment model.

--- spec/fabricators/assignment_fabricator.rb
+++ spec/fabricators/assignment_fabricator.rb
@@ -12,16 +12,16 @@ Fabricator(:assignment_with_quiz, :from => 'coursewareable/assignment') do
   quiz { [
     { :content => Faker::Lorem.sentence, :type => :text, :options => [
       { :content => Faker::Lorem.word, :valid => true }],
-    },
+    }.with_indifferent_access,
     { :content => Faker::Lorem.sentence, :type => :checkboxes, :options => [
       { :content => Faker::Lorem.word, :valid => true },
       { :content => Faker::Lorem.word, :valid => false },
       { :content => Faker::Lorem.word, :valid => true }]
-    },
+    }.with_indifferent_access,
     { :content => Faker::Lorem.sentence, :type => :radios, :options => [
       { :content => Faker::Lorem.word, :valid => true },
       { :content => Faker::Lorem.word, :valid => false },
       { :content => Faker::Lorem.word, :valid => false }]
-    },
+    }.with_indifferent_access,
   ] }
 end
